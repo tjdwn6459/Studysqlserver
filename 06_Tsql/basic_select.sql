9234a9bfe66fd46e1808d1d7c8e24fd511974485
tjdwn6459 <tjdwn6459@naver.com>
2021-02-05 16:46:51 +0900

    2021년 2월 5일 마지막 커밋

---
+++ 06_Tsql/basic_select.sql
@@ -0,0 +1,23 @@
+/*
+select memberID, memberName 
+ from membertbl
+ where memberID like 'H';
+ */
+ 
+ --select '1';
+
+--  use BikeStores;
+
+--  select * from sales.customers;
+ 
+--  select *  from production.products;
+
+ --DB조회
+ exec sp_helpdb;
+ --테이블 조회
+ exec sp_tables @table_type = "'table'";
+ --열이름 조회
+ exec sp_columns @table_name ='stocks',
+         @table_owner ='production';
+                 
+  
