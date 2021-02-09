65428711582e662681462aba6a8aa26051fa92e1
tjdwn6459 <tjdwn6459@naver.com>
2021-02-09 12:12:47 +0900

    20210209-12:12

---
+++ 06_Tsql/SQLQuery17.sql
@@ -0,0 +1,107 @@
+﻿use sqlDB;
+
+declare @myVar1 int;
+declare @myVar2 smallint , @myVar3 decimal(10,6);
+declare @myVar4 nchar(20);
+
+set @myVar1 = 5;
+set @myVar2 = 3;
+set @myVar3 = 3.141592;
+set @myVar4 = '가수 이름 ==>  ';
+
+
+select @myVar1;
+select  '100' + @myVar3; 
+select @myVar4, userName from userTbl where height > 180;
+
+declare @myVar5 tinyint; --255
+set @myVar5 = 3;
+
+select top(@myVar5) userName, height from userTbl order by height desc;
+
+
+select @@VERSION;
+
+--
+select convert (float, amount) from buyTbl;
+select TRY_CONVERT(float, amount) as amount from buyTbl;
+select avg(convert (decimal, amount)) as '평균구매개수' from buyTbl;
+
+
+select price, amount, price/amount as [단가/수량] from buyTbl;
+
+select avg ( amount) from buyTbl;
+
+--
+
+'1000' --->숫자
+select parse ('2021년 2월 39일' as date);
+select TRY_PARSE('2021년 2월 39일'as date);
+
+select parse ('123.45' as int);
+select try_parse ('123.45' as int);
+
+select parse ('123.45' as decimal(5,2));
+select try_parse ('123.45' as float);
+
+
+--스칼라함수(단일 리턴함수)\
+select GETDATE() as '현재날짜';
+
+select year(getdate()) as '금년';
+select month(getdate()) as '이번달';
+select day(getdate()) as '오늘일';
+
+select abs (-100);
+
+select round(3.1415, 3); 
+select round(1236.5678, -1);
+
+select cast((rand() * 100) as int );
+
+select iif(20 < 10, ' 참', '거짓');
+
+--문자열 함수
+select ascii('a'), ascii('A');
+select char(98), char(66);
+select unicode('나');
+select nchar(45909);
+
+
+
+---진짜 진짜 많이 쓰이는거
+--문자열 결합 함수
+select concat ('SQL','Server', 2019);
+--문자열 시작위치 리턴
+select CHARINDEX('2019', 'SQL Server 2019');
+--left, right
+select left ('SQL Server 2019' ,3), right('SQL Server 2019' ,4);
+
+--substring
+select substring('HELLO WORLD!', 7,5);
+select substring('안녕하세요, sql서버입니다!', 11, 2);
+--len
+select len('안녕하세요'), len('hello world!');
+select len('안녕하세요'), len('      hello world!');
+--LOWER, UPPER
+select lower('abcdeFG'), upper('abcdeFG');
+--ltrim,rtrim
+select len(ltrim ('    Hello World!    ')),
+       len (rtrim('    Hello World!   ')),
+	   len (trim ('    Hello World!    '));
+
+--replace
+select replace ('sql server 2016', '2016', '2019');
+
+--완전 많이 쓰는 함수
+select format(getdate(), ''); --기본
+select format(getdate(), 'dd/MM/yyyy');
+select format(getdate(), 'yyyy-MM-dd hh:mm:ss');
+select format(getdate(), 'yyyy-MM-dd');
+select format(getdate(), 'hh:mm:ss');
+select format(getdate(), '');
+select format(getdate(), '');
+select format(getdate(), '');
+select format(getdate(), '');
+select format(getdate(), '');
+	     
--- 06_Tsql/basic_select.sql
+++ 06_Tsql/basic_select.sql
@@ -21,3 +21,14 @@ select memberID, memberName
          @table_owner ='production';
                  
   
+INSERT into usertbl VALUES ('LSG', '이승기', 1987, '서울', '011','11111111', 182, '2008-8-8');
+INSERT into usertbl VALUES ('KBS', '김범수', 1979, '경남', '011','22222222', 173, '2012-4-4');
+INSERT into usertbl VALUES ('KKH', '김경호', 1971, '전남', '019','33333333', 177, '2007-7-7');
+INSERT into usertbl VALUES ('JYP', '박진영', 1950, '경기', '011','44444444', 166, '2013-12-12');
+INSERT into usertbl VALUES ('SSK', '성시경', 1979, '서울', NULL,NULL, 186, '2009-9-9');
+INSERT into usertbl VALUES ('LJB', '임재범', 1963, '서울', '016','55555555', 182, '2005-5-5');
+INSERT into usertbl VALUES ('YJS', '운종신', 1969, '경남', NULL,NULL, 170, '2014-3-3');
+INSERT into usertbl VALUES ('EJW', '은지원', 1972, '경북', '011','66666666', 174, '2010-10-10');
+INSERT into usertbl VALUES ('JKW', '조관우', 1965, '경기', '018','77777777', 172, '2013-5-5');
+INSERT into usertbl VALUES ('BBK', '바비킴', 1973, '서울', '010','88888888', 176, '2009-4-4');
+GO
